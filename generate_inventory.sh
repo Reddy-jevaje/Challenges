e6e1077857d3b850e62015861bc1fefbc7925022
Jathin_reddy <reddy.jevaje@gmail.com>
2024-12-18 15:24:40 +0530

    Delete generate_inventory.sh

--- generate_inventory.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cat <<EOF > inventory.ini
-[frontend]
-c8.local ansible_host=$(terraform output -raw amazon_linux_public_ip) ansible_user=ec2-user ansible_ssh_private_key_file=~/.ssh/your_key.pem
-
-[backend]
-u21.local ansible_host=$(terraform output -raw ubuntu_public_ip) ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/your_key.pem
-EOF
